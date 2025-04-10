2cd912d8879b52c338112fcabb2b30e079de6c63
markustare <mustare@byupathway.edu>
2025-04-10 21:49:21 +0400

    Shapes

---
+++ week06/Shapes/Circle.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Circle : Shape
+{
+    private float _radius;
+
+    public Circle(string color, float radius) : base(color)
+    {
+        _radius = radius;
+    }
+
+    public override float GetArea()
+    {
+        return (float)(Math.PI * _radius * _radius);
+    }
+}
